4e063c1e5e24ff634afafa8312b4ce04597b501c
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-02 14:29:59 +0530

    Add get product

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -1,5 +1,27 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 export function createProduct(req,res){
-    
+    if(! isAdmin(req)){
+        res.json({
+           message : "Please login as administrator to add products" 
+        })
+        return
+    }
+    const newProductData =req.body
+
+    Product.save().then(()=>{
+        res.json({
+            message : "Product created"
+        })
+    }).catch((error)=>{
+        res.json({
+            message : error
+        })
+    })
+}
+export function getProduct(req,res){
+    Product.find({}).then((Products)=>{
+    res.json(Products)
+})
 }
