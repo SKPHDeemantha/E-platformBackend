febb76cacf6bb0d4ee48e3b7a7806b974a868498
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-31 23:48:03 +0530

    feat: add product search functionality to retrieve products by name or alternative names

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProduct, getProductById, updateProduct } from '../controllers/productcontroller.js';
+import { createProduct, deleteProduct, getProduct, getProductById, searchProducts, updateProduct } from '../controllers/productcontroller.js';
 
 const productRouter =express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProduct)
+productRouter.get("/search/:query",searchProducts)
 productRouter.put("/:ProductId",updateProduct)
 productRouter.get("/:ProductId",getProductById)
 productRouter.delete("/:ProductId",deleteProduct)
